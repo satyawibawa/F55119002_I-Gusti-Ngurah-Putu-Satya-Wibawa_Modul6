641124ba4ab3159e0c2ac538a65a4654027fb276
satyawibawa <satyawibawa93@gmail.com>
2022-04-26 17:54:00 +0800

    Initial commit

---
+++ app/src/main/java/com/f55119002/satya_sqlite/AccountFragment.java
@@ -0,0 +1,6 @@
+package com.f55119002.satya_sqlite;
+
+import androidx.fragment.app.Fragment;
+
+public class AccountFragment extends Fragment {
+}
---
+++ app/src/main/java/com/f55119002/satya_sqlite/HomeFragment.java
@@ -0,0 +1,6 @@
+package com.f55119002.satya_sqlite;
+
+import androidx.fragment.app.Fragment;
+
+public class HomeFragment extends Fragment {
+}
